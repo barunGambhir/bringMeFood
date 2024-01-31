db7536d4e6f8ccf921439601f8e873f451acc0f5
bsg10 <bsg10@sfu.ca>
2024-01-30 23:00:51 -0800

    Initial Commit

---
+++ BringMeFood/Models/EmailSender.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace BringMeFood.Models
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessaage)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
